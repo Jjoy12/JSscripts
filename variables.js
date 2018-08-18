e3a2a6858373f2a07327724fae7d5cb9e33e5518
Jjoy12 <36667571+Jjoy12@users.noreply.github.com>
2018-08-18 16:46:17 -0500

    JS basics

---
+++ variables.js
@@ -0,0 +1,13 @@
+let firstName = 'Buddy';
+let lastName = 'Arnold';
+
+let fullName = firstName + ' ' +lastName;
+
+console.log(fullName);
+
+let score = 100;
+let bonus = 20;
+
+let totalScore = score + bonus;
+
+console.log(totalScore)
