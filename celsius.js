b11272f555e7765623aa6b87ecfe3a7e21787a71
Jjoy12 <36667571+Jjoy12@users.noreply.github.com>
2018-08-18 16:51:50 -0500

    Undefined and Boolean values practice

---
+++ celsius.js
@@ -0,0 +1,27 @@
+var score = 100;
+
+score = 130;
+
+const bonus = 50;
+
+var finalScore = (score + bonus) * 0.8 ;
+
+var complex = ((4 + 4) * 5) / 2;
+
+
+
+
+
+// console.log(finalScore);
+// console.log(complex);
+
+let tempInFarenheit = 100;
+
+// do caculation
+//(T(°F) - 32) × 5/9
+
+let celsius = (tempInFarenheit - 32) * 5/9;
+
+
+
+console.log(celsius);
